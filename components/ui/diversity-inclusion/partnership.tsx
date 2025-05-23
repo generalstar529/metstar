4ecb2f5119202f8df9e5ad3584d31d0a5cef4b61
generalstar529 <generalstar529@gmail.com>
2025-05-23 05:36:23 -0700

    Remove unused libraries

--- components/ui/diversity-inclusion/partnership.tsx
+++ components/ui/diversity-inclusion/partnership.tsx
@@ -1,50 +1,54 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Carousel from 'react-grid-carousel'
+import Image from "next/image";
+// import Carousel from 'react-grid-carousel'
 
-import imgAABE from '@/assets/images/diversity-aabe.webp'
-import imgWomen from '@/assets/images/diversity-women.webp'
-import imgAWE from '@/assets/images/diversity-awe.webp'
-import imgTexas from '@/assets/images/diversity-texas.webp'
-import imgNHCC from '@/assets/images/diversity-nhcc.webp'
+import imgAABE from "@/assets/images/diversity-aabe.webp";
+import imgWomen from "@/assets/images/diversity-women.webp";
+import imgAWE from "@/assets/images/diversity-awe.webp";
+import imgTexas from "@/assets/images/diversity-texas.webp";
+import imgNHCC from "@/assets/images/diversity-nhcc.webp";
 
 interface IPartner {
-  src: any,
+  src: any;
   alt: string;
 }
 
-const partnerships : IPartner[] = [
-  { src: imgAABE, alt: 'AABE',},
-  { src: imgWomen, alt: 'Women',},
-  { src: imgAWE, alt: 'AWE',},
-  { src: imgTexas, alt: 'TEXAS',},
-  { src: imgNHCC, alt: 'NHCC',},
-]
+const partnerships: IPartner[] = [
+  { src: imgAABE, alt: "AABE" },
+  { src: imgWomen, alt: "Women" },
+  { src: imgAWE, alt: "AWE" },
+  { src: imgTexas, alt: "TEXAS" },
+  { src: imgNHCC, alt: "NHCC" },
+];
 
 export default function Partnership() {
-
   return (
     <section className="diversity-partnership">
-      <div className='container text-left mx-auto'>
-        <div className='bg-zinc-100 p-20 w-10/12 mx-auto'>
+      <div className="container text-left mx-auto">
+        <div className="bg-zinc-100 p-20 w-10/12 mx-auto">
           <div className="title text-4xl md:text-5xl mb-16 text-black font-bold">
-            DIVERSITY & INCLUSION<br/>PARTNERSHIPS
+            DIVERSITY & INCLUSION
+            <br />
+            PARTNERSHIPS
           </div>
-          <div className='text-sm text-black mb-16'>
-            We are proud to partner with organizations that value diversity and are committed to full inclusion in their sectors.
+          <div className="text-sm text-black mb-16">
+            We are proud to partner with organizations that value diversity and
+            are committed to full inclusion in their sectors.
           </div>
-          <Carousel cols={3} rows={1} gap={10} showDots={true} loop>
-            {
-              partnerships.map((partnerItem, idx) => (
-                <Carousel.Item key={idx}>
-                  <Image src={partnerItem.src} alt={partnerItem.alt} className='partner-image'></Image>
-                </Carousel.Item>
-              ))
-            }
-          </Carousel>
+          {/* <Carousel cols={3} rows={1} gap={10} showDots={true} loop>
+            {partnerships.map((partnerItem, idx) => (
+              <Carousel.Item key={idx}>
+                <Image
+                  src={partnerItem.src}
+                  alt={partnerItem.alt}
+                  className="partner-image"
+                ></Image>
+              </Carousel.Item>
+            ))}
+          </Carousel> */}
         </div>
       </div>
     </section>
-  )
-} 
+  );
+}
