60c9a78166a0360c11cf9c5399bb4926042625b5
generalstar529 <generalstar529@gmail.com>
2025-06-18 11:33:39 -0700

    Format some component codes

--- components/ui/leadership/history.tsx
+++ components/ui/leadership/history.tsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
 export default function History() {
-
   return (
     <section className="leadership-history">
-      <div className='container mx-auto grid text-center lg:w-6/12'>
+      <div className="container mx-auto grid text-center lg:w-6/12">
         <div className="title flex text-xl md:text-2xl font-bold my-20">
-          Metstar Premier Era Innovations was built through a history of strong, innovative, and entrepreneurial leaders.
+          Metstar Premier Era Innovations was built through a history of strong,
+          innovative, and entrepreneurial leaders.
         </div>
       </div>
     </section>
-  )
-} 
+  );
+}
